167c43e7c1819f3aa324ff9d9f454079bb5148ed
Bikki <bikrambhusal75@gmail.com>
2023-08-02 13:37:50 +0545

    Conditional Statements

--- client/src/components/Example.js
+++ client/src/components/Example.js
@@ -1,14 +1,32 @@
-//rfce
+//rfce for boiler plate
 
-function Example() {
-  const shoot = (a) => {
-    alert(a);
-  };
-  return (
-    <>
-      <button onClick={() => shoot("Goal!!!!!!")}>Take the shot!</button>
-    </>
-  );
+// function Example() {
+//   const shoot = (a) => {
+//     alert(a);
+//   };
+//   return (
+//     <>
+//       <button onClick={() => shoot("Goal!!!!!!")}>Take the shot!</button>
+//     </>
+//   );
+// }
+
+// export default Example;
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  if (isGoal) {
+    return <MadeGoal />;
+  }
+  return <MissedGoal />;
 }
 
-export default Example;
+function MadeGoal() {
+  return <h1>Goal!!</h1>;
+}
+function MissedGoal() {
+  return <h1>MISSED!</h1>;
+}
+
+export default Goal;
+
